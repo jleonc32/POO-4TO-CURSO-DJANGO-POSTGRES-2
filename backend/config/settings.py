a9ee254b3428da9d3b7affd97d94a6cb40c340dd
Juan Pablo Leon <jleonc32@unemi.edu.ec>
2026-07-09 00:04:36 -0500

    modificar use_tz a Flase-corregir facturas hoy, ventas mes

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -96,7 +96,7 @@ AUTH_PASSWORD_VALIDATORS = [
 LANGUAGE_CODE = "es-ec"
 TIME_ZONE = "America/Guayaquil"
 USE_I18N = True
-USE_TZ = True
+USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
